912ccbfa27832cec1366516ef99d09cb4d5f87ea
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-03-26 12:29:23 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//  Exiba a palavra como se fosse soletrada.
+
+String Palavra;
+String PalavraSoletrada;
+
+Console.WriteLine("----Soletrar----\n");
+
+Console.Write("Digite uma palavra:");
+Palavra = Console.ReadLine()!;
+
+var caracteres = Palavra.ToCharArray();
+PalavraSoletrada = String.Join('-', caracteres).ToUpper();
+
+Console.WriteLine($"\nSoletrando \"{Palavra.ToUpper()}\": {PalavraSoletrada}");
